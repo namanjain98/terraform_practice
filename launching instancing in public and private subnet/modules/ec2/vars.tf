eed19efaaabe0fdeea8edd3f98d43893abd53824
naman <naman98.j@gmail.com>
2020-07-22 12:25:20 +0530

    final changes in key pair in launching instance using public and private key and removed all errors

--- launching instancing in public and private subnet/modules/ec2/vars.tf
+++ launching instancing in public and private subnet/modules/ec2/vars.tf
@@ -2,3 +2,11 @@ variable "subnet_id"{
         }                
 variable "vpc_id"{       
 }                        
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
